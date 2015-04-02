1d01cda64cca503020d39975668aedaf0ca456bf
Patrick Farrell <mr.patfarrell@gmail.com>
2015-04-01 23:53:46 -0700

    simple extraction support

--- src/main/java/org/boilerpipe/web/BoilerpipeService.java
+++ src/main/java/org/boilerpipe/web/BoilerpipeService.java
@@ -8,6 +8,13 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.net.URL;
+import de.l3s.boilerpipe.BoilerpipeExtractor;
+import de.l3s.boilerpipe.document.Media;
+import de.l3s.boilerpipe.extractors.CommonExtractors;
+import de.l3s.boilerpipe.sax.HtmlArticleExtractor;
+import de.l3s.boilerpipe.sax.MediaExtractor;
+
 
 @Path("/")
 public class BoilerpipeService {
@@ -17,7 +24,22 @@ public class BoilerpipeService {
   @Produces("text/json")
   public Map<String, String> getContent() {
     Map<String, String> cont = new HashMap<String, String>();
-    cont.put("test", "Oh... hello");
+    String u="http://www.spiegel.de/wissenschaft/natur/0,1518,789176,00.html";
+    cont.put("url", u);
+
+    try {
+		URL url = new URL(u);
+
+		final BoilerpipeExtractor extractor = CommonExtractors.ARTICLE_EXTRACTOR;
+
+		final HtmlArticleExtractor htmlExtr = HtmlArticleExtractor.INSTANCE;
+		
+		String html = htmlExtr.process(extractor, url);
+		
+    cont.put("content", html);
+    }catch(Exception ex) {
+      ex.printStackTrace();
+    }
 
     return cont;
   }
